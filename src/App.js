f8487da48f18900dce321a173d04af96d1ba4565
andyxie <andrewcrab.china@gmail.com>
2017-05-01 15:18:06 -0700

    Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.js'
+
+var PLAYERS = [
+  {
+    name: "Jim Hoskins",
+    score: 31,
+    id: 1,
+  },
+  {
+    name: "Andrew Chalkley",
+    score: 35,
+    id: 2,
+  },
+  {
+    name: "Alena Holligan",
+    score: 42,
+    id: 3,
+  },
+];
 
 class App extends Component {
   render() {
     return (
       <div className="application">
-        <div className="header">
-          <h1>Scoreboard</h1>
-        </div>
+        <Header title="My Score Board"/>
 
         <div className="players">
           <div className="player">
