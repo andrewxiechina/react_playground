ee6c0554b0e84aef0d7250ee2f505db4992a629c
andyxie <andrewcrab.china@gmail.com>
2017-05-01 19:38:50 -0700

    small change

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../css/App.css';
+import '../css/Scoreboard.css';
 import Header from './Header.js'
 import Player from './Player.js'
 import Counter from './Counter.js'
